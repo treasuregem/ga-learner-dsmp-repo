2431689ad339b80288d8cfd321822b9cabbaed1a
treasuregem <40883548+treasuregem@users.noreply.github.com>
2019-04-16 21:29:37 +0530

    Update code.py
    used cross_validation.train_test_split

--- Forest_cover_Feature_Selection/code.py
+++ Forest_cover_Feature_Selection/code.py
@@ -83,24 +83,28 @@
 	#Import libraries 
 	from sklearn import cross_validation
 	from sklearn.preprocessing import StandardScaler
-	from sklearn.model_selection import train_test_split
+
 	# Identify the unnecessary columns and remove it 
 	dataset.drop(columns=['Soil_Type7', 'Soil_Type15'], inplace=True)
-	X=dataset.drop('Cover_Type',axis=1)
-	Y=dataset['Cover_Type']
-	X_train, X_test, Y_train, Y_test = train_test_split(X,Y,random_state=0,test_size=0.2)
-	
+
+	r,c = dataset.shape
+	X = dataset.iloc[:,:-1]
+	Y = dataset.iloc[:,-1]
+
 	# Scales are not the same for all variables. Hence, rescaling and standardization may be necessary for some algorithm to be applied on it.
-	
+	X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.2, random_state=0)
+
+
+
 	#Standardized
 	#Apply transform only for non-categorical data
-	scale=StandardScaler()
-	X_train_temp = scale.fit_transform(X_train.iloc[:,:10])
-	X_test_temp = scale.fit_transform(X_test.iloc[:,:10])
-	
+	X_train_temp = StandardScaler().fit_transform(X_train.iloc[:,:10])
+	X_test_temp = StandardScaler().fit_transform(X_test.iloc[:,:10])
+
 	#Concatenate non-categorical data and categorical
-	X_train1=numpy.concatenate([X_train_temp,X_train.iloc[:,10:]],axis=1)
-	X_test1=numpy.concatenate([X_test_temp,X_test.iloc[:,10:]],axis=1)
+	X_train1 = numpy.concatenate((X_train_temp,X_train.iloc[:,10:c-1]),axis=1)
+	X_test1 = numpy.concatenate((X_test_temp,X_test.iloc[:,10:c-1]),axis=1)
+
 	scaled_features_train_df = pd.DataFrame(X_train1, index=X_train.index, columns=X_train.columns)
 	scaled_features_test_df = pd.DataFrame(X_test1, index=X_test.index, columns=X_test.columns)
 #----------------------------------------------------------------
